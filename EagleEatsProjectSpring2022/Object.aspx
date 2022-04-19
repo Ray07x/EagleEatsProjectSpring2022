f385382a7987f6fa7cadcb9c433b74b379edc1b6
JohnMessmer <jmessmer37@gmail.com>
2022-04-19 14:33:51 -0500

    eagle eats project
    cs458

---
+++ EagleEatsProjectSpring2022/Object.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Object.aspx.cs" Inherits="EagleEatsProjectSpring2022.Object" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Object</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ EagleEatsProjectSpring2022/Route.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EagleEatsProjectSpring2022
+{
+    public partial class Route : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void FormView1_ItemUpdated(object sender, FormViewUpdatedEventArgs e)
+        {
+            
+            this.GridView1.DataBind();
+        }
+
+        protected void FormView1_ItemInserted(object sender, FormViewInsertedEventArgs e)
+        {
+            this.GridView1.DataBind();
+        }
+    }
+}
